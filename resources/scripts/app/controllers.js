eba743a033762779c3abc2555ced1448d04ead6a
hongquanli <hongquanli@qq.com>
2014-03-14 16:17:54 +0800

    Signed-off-by: hongquanli <hongquanli@qq.com>

--- resources/scripts/app/controllers.js
+++ resources/scripts/app/controllers.js
@@ -6,4 +6,28 @@ angular.module('myApp.controllers', []).controller('MyCtrl1', [ function() {
 
 } ]).controller('MyCtrl2', [ function() {
 
-} ]);
+} ]).controller('MyController', function($scope) {
+
+	$scope.person = {
+		name : "Ari Lerner"
+	};
+}).controller('AlertDemoCtrl', function($scope) {
+
+	$scope.alerts = [ {
+		type : 'danger',
+		msg : 'Oh snap! Change a few things up and try submitting again.'
+	}, {
+		type : 'success',
+		msg : 'Well done! You successfully read this important alert message.'
+	} ];
+
+	$scope.addAlert = function() {
+		$scope.alerts.push({
+			msg : "Another alert!"
+		});
+	};
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+});
